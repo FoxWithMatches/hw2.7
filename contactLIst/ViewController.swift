36af24b0a8d04d395c7f1f70e858063845c3d5b7
Alisa Ts <freiheiit92@mail.ru>
2021-11-09 21:09:56 +0300

    Initial Commit

---
+++ contactLIst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contactLIst
+//
+//  Created by Alisa Ts on 09.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
